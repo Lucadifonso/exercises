c58cd5553aba2ba6727272467397f3c0b3ca7bad
Lucadifonso <90312038+Lucadifonso@users.noreply.github.com>
2021-10-05 14:08:33 +0100

    Sort a list of number moving the bigger number to the right

---
+++ Bouble Sort Loops.py
@@ -0,0 +1,39 @@
+import random
+
+num_list = []
+for i in range(5):
+    num_list.append(random.randrange(1,9))
+
+for i in num_list:
+    print(i, end="")
+
+i = len(num_list) - 1
+while i >1:
+    j = 0
+    while j < i:
+        print("\nIs {} > {}". format(num_list[j],num_list[j+1]))
+        print()
+        if num_list[j] > num_list[j+1]:
+            print("SWITCH ")
+            temp = num_list[j]
+            num_list[j] = num_list[j+1]
+            num_list[j+1] = temp
+        else:
+            print("Do not switch ")
+
+        j += 1
+        for k in num_list:
+            print(k, end="")
+        print()
+
+    print("End of Round")
+    i -= 1
+    for k in num_list:
+        print(k, end="")
+    print()
+
+
+
+
+for i in num_list:
+    print(i)
